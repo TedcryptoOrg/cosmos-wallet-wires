3731b01dcdef5acd1f7f97efe4f175c98e54c534
Jose Lopes <josluis.lopes@gmail.com>
2023-02-14 14:24:51 +0000

    Publish cosmos wallet wires

--- src/WalletConnector.ts
+++ src/WalletConnector.ts
@@ -1,3 +1,5 @@
+import { Wallet } from "./Wallet";
+
 export default class WalletConnector {
     private wallet: Wallet;
 
---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import KeplrWallet from "./providers/KeplrWallet";
+import MetaMaskWallet from "./providers/MetaMaskWallet";
+import { Wallet } from "./Wallet";
+import WalletConnector from "./WalletConnector";
+
+export {
+    // Classes
+    WalletConnector,
+    Wallet,
+    // Providers
+    KeplrWallet,
+    MetaMaskWallet,
+}
